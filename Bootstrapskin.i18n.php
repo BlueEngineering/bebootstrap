e2869c8c09b32701f94284da6411592ffacf8ee8
tjaap <tim.jaap@mailbox.tu-berlin.de>
2015-05-18 00:31:06 +0200

    Frist upload

---
+++ Bootstrapskin.i18n.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Internationalisation file for skin BootstrapSkin
+ *
+ * @file
+ * @ingroup Skins
+ */
+
+$messages = array();
+
+/** English
+ * @author Lee Miller
+ */
+$messages['en'] = array(
+	'skinname-bootstrapskin' => "bootstrapskin",
+	'bootstrapskin-desc' => "Bootstrap styled skin for Mediawiki",
+);
