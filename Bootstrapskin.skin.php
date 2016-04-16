d5438269470ee461d484651aa1e1df6d9ad345c4
anderer <andrebaier@gmail.com>
2016-04-16 22:33:42 +0200

    php menu

--- Bootstrapskin.skin.php
+++ Bootstrapskin.skin.php
@@ -172,7 +172,7 @@ class StrappingTemplate extends BaseTemplate {
             <div id="defaultmenu" class="navbar-collapse collapse col-md-8 pull-left"> 
                 <ul class="nav navbar-nav">
                     <!-- Mega Menu -->
-<li class="dropdown hadron-fw"><a data-description="" href="Grundgedanken">Grundgedanken</a>
+<li class="dropdown hadron-fw"><a data-description="" href="Über uns">Über uns</a>
 
 <li class="dropdown hadron-fw"><a href="#" data-toggle="dropdown" class="dropdown-toggle">Berlin<b class="caret"></b></a>
                         <ul class="dropdown-menu fullwidth">
@@ -237,7 +237,6 @@ class StrappingTemplate extends BaseTemplate {
                                         <li aria-haspopup="true"><?php if ( !isset( $portals['TOOLBOX'] ) ) {$this->renderNavigation( array( 'TOOLBOX' ) );?></li>
                                         </ul>
                                     </div>
-                                    
                                 </div><!-- end row -->                               
                             </li><!-- end grid demo -->
                         </ul><!-- end drop down menu -->
