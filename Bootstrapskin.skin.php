65cc41a63ff076c12c7c15e3c747a4fdd13603ee
anderer <andrebaier@gmail.com>
2016-10-17 16:17:35 +0200

    also doch mit : )

--- Bootstrapskin.skin.php
+++ Bootstrapskin.skin.php
@@ -186,8 +186,8 @@ class StrappingTemplate extends BaseTemplate {
                                     <div class="col-sm-3">
                                         <ul>
                                         <li><h3 class="title">Projektwerkstatt</h3></li>
-                                        <li><a data-description="" href="/TUB:Allgemein">Allgemeine Informationen</a></li>
-                                        <li><a data-description="" href="/TUB:PW:2016_2">Projektwerkstatt WS 2016/2017</a></li>        
+                                        <li><a data-description="" href=":TUB:Allgemein">Allgemeine Informationen</a></li>
+                                        <li><a data-description="" href=":TUB:PW:2016_2">Projektwerkstatt WS 2016/2017</a></li>        
                                         </ul>
                                     </div>
                                     <div class="col-sm-3">
